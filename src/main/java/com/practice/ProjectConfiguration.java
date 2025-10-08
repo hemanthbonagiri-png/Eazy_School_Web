d53236803a477578087259d9b46616dd2ba82d9a
hemanth bona <hemanthbonagiri1994@gmail.com>
2025-10-08 12:21:03 +0100

    Initial commit of java project

---
+++ src/main/java/com/practice/ProjectConfiguration.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+@ComponentScan(basePackages = "com.practice")
+public class ProjectConfiguration {
+
+}
---
+++ src/main/java/com/practice/Vehicle.java
@@ -0,0 +1,27 @@
+package com.practice;
+
+
+public class Vehicle {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void carBean(){
+
+        System.out.println("Car has been created");
+    }
+
+
+
+
+
+
+}
